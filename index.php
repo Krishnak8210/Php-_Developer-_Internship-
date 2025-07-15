a3b5e47e8efeae80b620c2c8e821c755bb28fd3d
Krishnak8210 <146201975+Krishnak8210@users.noreply.github.com>
2025-07-15 13:47:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h1 style='color: red; font-size: 40px; justfy-contect: center; '>Hello ApexPlanet </h1> <br>";
+
+echo "<h3>About Us</h3>";
+echo "<p >At ApexPlanet Software Pvt. Ltd., we are dedicated to driving innovation and excellence in the realm of
+digital solutions. Founded with a vision to empower businesses through cutting-edge technology, we
+specialize in delivering top-notch web development and app development services tailored to meet the
+unique needs of our clients. </p>";
+
+
+
+?>
